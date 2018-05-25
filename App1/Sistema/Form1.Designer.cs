40bc2a6eb95423d2bd78782f79c1e2fdd994015d
SergioMArinM <sergiomarin1985@gmail.com>
2018-05-24 20:18:54 -0400

    creacionde formularios

---
+++ App1/Sistema/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace Sistema
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBoxMarca = new System.Windows.Forms.ComboBox();
+            this.guardar = new System.Windows.Forms.Button();
+            this.Cod_barra = new System.Windows.Forms.Label();
+            this.Nombre = new System.Windows.Forms.Label();
+            this.Un_medida = new System.Windows.Forms.Label();
+            this.Marca = new System.Windows.Forms.Label();
+            this.formato = new System.Windows.Forms.Label();
+            this.linea = new System.Windows.Forms.Label();
+            this.familia = new System.Windows.Forms.Label();
+            this.comboBoxLinea = new System.Windows.Forms.ComboBox();
+            this.comboBoxFamilia = new System.Windows.Forms.ComboBox();
+            this.textcodbarra = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textunmedidad = new System.Windows.Forms.TextBox();
+            this.textformato = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // comboBoxMarca
+            // 
+            this.comboBoxMarca.FormattingEnabled = true;
+            this.comboBoxMarca.Location = new System.Drawing.Point(217, 172);
+            this.comboBoxMarca.Name = "comboBoxMarca";
+            this.comboBoxMarca.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxMarca.TabIndex = 0;
+            // 
+            // guardar
+            // 
+            this.guardar.Location = new System.Drawing.Point(258, 313);
+            this.guardar.Name = "guardar";
+            this.guardar.Size = new System.Drawing.Size(75, 23);
+            this.guardar.TabIndex = 1;
+            this.guardar.Text = "Guardar";
+            this.guardar.UseVisualStyleBackColor = true;
+            // 
+            // Cod_barra
+            // 
+            this.Cod_barra.AutoSize = true;
+            this.Cod_barra.Location = new System.Drawing.Point(52, 81);
+            this.Cod_barra.Name = "Cod_barra";
+            this.Cod_barra.Size = new System.Drawing.Size(83, 13);
+            this.Cod_barra.TabIndex = 2;
+            this.Cod_barra.Text = "Codigo de Barra";
+            // 
+            // Nombre
+            // 
+            this.Nombre.AutoSize = true;
+            this.Nombre.Location = new System.Drawing.Point(52, 113);
+            this.Nombre.Name = "Nombre";
+            this.Nombre.Size = new System.Drawing.Size(44, 13);
+            this.Nombre.TabIndex = 3;
+            this.Nombre.Text = "Nombre";
+            this.Nombre.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Un_medida
+            // 
+            this.Un_medida.AutoSize = true;
+            this.Un_medida.Location = new System.Drawing.Point(52, 140);
+            this.Un_medida.Name = "Un_medida";
+            this.Un_medida.Size = new System.Drawing.Size(93, 13);
+            this.Un_medida.TabIndex = 4;
+            this.Un_medida.Text = "Unidad de medida";
+            this.Un_medida.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Marca
+            // 
+            this.Marca.AutoSize = true;
+            this.Marca.Location = new System.Drawing.Point(52, 172);
+            this.Marca.Name = "Marca";
+            this.Marca.Size = new System.Drawing.Size(37, 13);
+            this.Marca.TabIndex = 5;
+            this.Marca.Text = "Marca";
+            // 
+            // formato
+            // 
+            this.formato.AutoSize = true;
+            this.formato.Location = new System.Drawing.Point(52, 217);
+            this.formato.Name = "formato";
+            this.formato.Size = new System.Drawing.Size(45, 13);
+            this.formato.TabIndex = 6;
+            this.formato.Text = "Formato";
+            this.formato.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // linea
+            // 
+            this.linea.AutoSize = true;
+            this.linea.Location = new System.Drawing.Point(55, 254);
+            this.linea.Name = "linea";
+            this.linea.Size = new System.Drawing.Size(33, 13);
+            this.linea.TabIndex = 7;
+            this.linea.Text = "Linea";
+            // 
+            // familia
+            // 
+            this.familia.AutoSize = true;
+            this.familia.Location = new System.Drawing.Point(58, 287);
+            this.familia.Name = "familia";
+            this.familia.Size = new System.Drawing.Size(39, 13);
+            this.familia.TabIndex = 8;
+            this.familia.Text = "Familia";
+            // 
+            // comboBoxLinea
+            // 
+            this.comboBoxLinea.FormattingEnabled = true;
+            this.comboBoxLinea.Location = new System.Drawing.Point(217, 245);
+            this.comboBoxLinea.Name = "comboBoxLinea";
+            this.comboBoxLinea.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxLinea.TabIndex = 9;
+            // 
+            // comboBoxFamilia
+            // 
+            this.comboBoxFamilia.FormattingEnabled = true;
+            this.comboBoxFamilia.Location = new System.Drawing.Point(217, 278);
+            this.comboBoxFamilia.Name = "comboBoxFamilia";
+            this.comboBoxFamilia.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxFamilia.TabIndex = 10;
+            // 
+            // textcodbarra
+            // 
+            this.textcodbarra.Location = new System.Drawing.Point(217, 81);
+            this.textcodbarra.Name = "textcodbarra";
+            this.textcodbarra.Size = new System.Drawing.Size(100, 20);
+            this.textcodbarra.TabIndex = 11;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(217, 113);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(248, 20);
+            this.textBox2.TabIndex = 12;
+            // 
+            // textunmedidad
+            // 
+            this.textunmedidad.Location = new System.Drawing.Point(217, 140);
+            this.textunmedidad.Name = "textunmedidad";
+            this.textunmedidad.Size = new System.Drawing.Size(100, 20);
+            this.textunmedidad.TabIndex = 13;
+            // 
+            // textformato
+            // 
+            this.textformato.Location = new System.Drawing.Point(217, 209);
+            this.textformato.Name = "textformato";
+            this.textformato.Size = new System.Drawing.Size(100, 20);
+            this.textformato.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textformato);
+            this.Controls.Add(this.textunmedidad);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textcodbarra);
+            this.Controls.Add(this.comboBoxFamilia);
+            this.Controls.Add(this.comboBoxLinea);
+            this.Controls.Add(this.familia);
+            this.Controls.Add(this.linea);
+            this.Controls.Add(this.formato);
+            this.Controls.Add(this.Marca);
+            this.Controls.Add(this.Un_medida);
+            this.Controls.Add(this.Nombre);
+            this.Controls.Add(this.Cod_barra);
+            this.Controls.Add(this.guardar);
+            this.Controls.Add(this.comboBoxMarca);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxMarca;
+        private System.Windows.Forms.Button guardar;
+        private System.Windows.Forms.Label Cod_barra;
+        private System.Windows.Forms.Label Nombre;
+        private System.Windows.Forms.Label Un_medida;
+        private System.Windows.Forms.Label Marca;
+        private System.Windows.Forms.Label formato;
+        private System.Windows.Forms.Label linea;
+        private System.Windows.Forms.Label familia;
+        private System.Windows.Forms.ComboBox comboBoxLinea;
+        private System.Windows.Forms.ComboBox comboBoxFamilia;
+        private System.Windows.Forms.TextBox textcodbarra;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textunmedidad;
+        private System.Windows.Forms.TextBox textformato;
+    }
+}

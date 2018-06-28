9b7d64db5044dcf2b3d0c5b1b8ac2034b4ab5101
JuanOjedaGomez <39174501+JuanOjedaGomez@users.noreply.github.com>
2018-06-28 01:00:57 -0400

    REPARACION REPORTES

--- App1/Sistema/FormReporteDiario.cs
+++ App1/Sistema/FormReporteDiario.cs
@@ -42,38 +42,45 @@ namespace Sistema
             tabla.Columns.Add("Rentabilidad $");
             tabla.Columns.Add("Rentabilidad %");
 
-            //MessageBox.Show("Esto trae el combobox: "+ tp_inicio.Value, "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            //dg_reporte.Rows.Clear();
 
-            foreach (var pd in db.produccion_diaria.ToList())
+            Boolean se_encontro = false;
+            DateTime f_inicio = tp_inicio.Value.Date;
+            DateTime f_fin = tp_fin.Value.Date;
+
+
+            if (f_inicio > f_fin)
             {
-                if (tp_inicio.Value > tp_fin.Value)
+                MessageBox.Show("La fecha de inicio no puede ser mayor a la fecha de termino ", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            else
+            {
+                foreach (var pd in db.produccion_diaria.ToList())
                 {
-                    MessageBox.Show("La fecha de inicio no puede ser mayor a la fecha de termino ", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
-                    break;
-                }
-                else {
-                    if ((tp_inicio.Value < pd.fecha)&&(tp_fin.Value > pd.fecha)){
+
+                    if ((f_inicio <= pd.fecha) && (f_fin >= pd.fecha))
+                    {
+                        se_encontro = true;
                         DataRow row = tabla.NewRow();
                         row["Fecha"] = Convert.ToString(pd.fecha);
                         var costo = db.costo.FirstOrDefault(codigo => codigo.producto_id == pd.producto_id);
-                        row["Precio Costo"] = costo.valor+costo.valor;
+                        row["Precio Costo"] = costo.valor;
                         var venta = db.precio_venta.FirstOrDefault(codigo => codigo.producto_id == pd.producto_id);
-                        row["Precio Venta"] = venta.valor+venta.valor;
+                        //row["Precio Venta"] = venta.valor;
                         row["Rentabilidad $"] = (venta.valor - costo.valor);
                         Decimal rentabilidad = (((Convert.ToDecimal(venta.valor) / costo.valor) - 1) * 100);
                         row["Rentabilidad %"] = rentabilidad.ToString("###,###.##");
                         tabla.Rows.Add(row);
-                    }else{
-                        MessageBox.Show("No se encuentan registros en el rango de fechas indicado", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
-                        break;
                     }
                 }
             }
-
+            if (se_encontro == false)
+            {
+                MessageBox.Show("No se encontraron registros ", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
             dg_reporte.DataSource = tabla;
             dg_reporte.Refresh();
         }
-
         private void FormReporteDiario_Load(object sender, EventArgs e)
         {
 

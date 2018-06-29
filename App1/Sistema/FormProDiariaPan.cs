79e0d4e39f17825ead0134014bd15e9e2dcb439e
Usuario1 <sergiomarin1985@gmail.com>
2018-06-29 19:21:15 -0400

    arreglo formularios boton

--- App1/Sistema/FormProDiariaPan.cs
+++ App1/Sistema/FormProDiariaPan.cs
@@ -12,146 +12,334 @@ namespace Sistema
 {
     public partial class FormProDiariaPan : Form
     {
-           public void nombre_producto()
+        public FormProDiariaPan()
+        {
+            InitializeComponent();
+            listaProductos();
+            listaUniMed();
+            tb_produccion.Focus();
+            mostrar_datagridview();
+        }
 
+
+        public void listaProductos()
         {
             costeoEntities db = new costeoEntities();
             var lista = db.producto.ToList();
             if (lista.Count > 0)
             {
-                cm_prodfinal.DataSource = lista;
-                cm_prodfinal.DisplayMember = "nombre";
-                cm_prodfinal.ValueMember = "id";
+                cm_producto.DataSource = lista;
+                cm_producto.DisplayMember = "nombre";
+                cm_producto.ValueMember = "id";
             }
-
         }
-        public FormProDiariaPan()
+
+        public void listaUniMed()
         {
-            InitializeComponent();
-            nombre_producto();
-            mostrar3_datagridview();
+            costeoEntities db = new costeoEntities();
+            var lista = db.unidad_medida.ToList();
+            if (lista.Count > 0)
+            {
+                cm_unimed.DataSource = lista;
+                cm_unimed.DisplayMember = "descripcion";
+                cm_unimed.ValueMember = "id";
+            }
         }
 
-        private void FormProDiariaPan_Load(object sender, EventArgs e)
-        {
 
-        }
-
-        private void label3_Click(object sender, EventArgs e)
-        {
-
-        }
-        private void mostrar3_datagridview()
-        {
-            costeoEntities precio_diario = new costeoEntities();
-            DataTable tabla = new DataTable();
-            tabla.Columns.Add("producto");
-            tabla.Columns.Add("uni de medida");
-            tabla.Columns.Add("produccion");
-            tabla.Columns.Add("fecha");
-            tabla.Columns.Add("Id");
-            /* foreach (var dato in precio_diario.precio_venta.ToList())
-             {
-                 if (dato.eliminado_el == null)
-                 {
-                     DataRow row = tabla.NewRow();
-                     row["Fecha"] = Convert.ToString(dato.fecha);
-                     costeoEntities db = new costeoEntities();
-                     var producto = db.producto.FirstOrDefault(codigo => codigo.id == dato.producto_id);
-                     row["Producto"] = producto.nombre;
-                     row["Precio"] = Convert.ToString(dato.valor);
-                     row["Id"] = Convert.ToString(dato.id);
-                     tabla.Rows.Add(row);
-                 }
-
-             }*/
-            tabla.DefaultView.Sort = "[Fecha] DESC";
-            dg_reporte.DataSource = tabla;
-            dg_reporte.Columns["Producto"].Width = 226;
-            dg_reporte.Columns["Id"].Visible = false;
-            dg_reporte.Refresh();
-        }
-        private void dg_reporte_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
-          
-
-        }
 
         private void bt_guardar_Click(object sender, EventArgs e)
         {
-            
-        }
-
-        private void cm_prodfinal_SelectedIndexChanged(object sender, EventArgs e)
-        {
-          
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            if (tb_unidadmedida.Text == "")
-            {
-                // MessageBox.Show("Debe Ingresar Descripcion Familia");
-                MessageBox.Show("Debe Ingresar unidad de medida ", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
-                tb_unidadmedida.Focus();
-            }
             if (tb_produccion.Text == "")
             {
-                // MessageBox.Show("Debe Ingresar Descripcion Familia");
-                MessageBox.Show("Debe Ingresar produccion ", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                MessageBox.Show("Debe Ingresar Produccion", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
                 tb_produccion.Focus();
+
             }
-            costeoEntities db = new costeoEntities();
-            produccion_diaria pro = new produccion_diaria();
+            else
+            {
 
-            pro.producto_id = Convert.ToInt32(cm_prodfinal.SelectedValue);
-            pro.unidad_medida = tb_unidadmedida.Text;
-            pro.produccion = tb_produccion.Text;
-            pro.creado_el = DateTime.Today;
-            db.produccion_diaria.Add(pro);
-            db.SaveChanges();
-            MessageBox.Show("Registro Guardado", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
-            tb_unidadmedida.Focus();
-            mostrar3_datagridview();
+                if (tb_id.Text == "")
+                {
+                    costeoEntities dbp = new costeoEntities();
+                    String fecval_ex = "N";
+                    foreach (var dato in dbp.produccion_diaria.ToList())
+                    {
+                        if ((dato.fecha == DateTime.Today) && (dato.produccion == Convert.ToString(tb_produccion.Text)))
+                        {
+                            fecval_ex = "S";
+                        }
+                    }
+                    if (fecval_ex == "S")
+                    {
+                        MessageBox.Show("Registro Ya Existe", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                        tb_produccion.Text = "";
+                        tb_id.Text = "";
+                        tb_produccion.Focus();
+                        mostrar_datagridview();
+                    }
+                    else
+                    {
+                        costeoEntities db = new costeoEntities();
+                        producto pro = new producto();
+                        var codigo_pro = Convert.ToInt16(cm_producto.SelectedValue);
+                        unidad_medida um = new unidad_medida();
+                        var codigo_um = Convert.ToInt16(cm_unimed.SelectedValue);
+                        var unimed = db.unidad_medida.FirstOrDefault(codigol => codigol.id == codigo_um);
+                        produccion_diaria pd = new produccion_diaria();
+                        pd.unidad_medida = Convert.ToString(unimed.descripcion);
+                        pd.produccion = Convert.ToString(tb_produccion.Text);
+                        pd.fecha = DateTime.Today;
+                        pd.producto_id = Convert.ToInt16(codigo_pro);
+                        pd.creado_el = DateTime.Today;
+                        pd.modicifado_el = null;
+                        pd.eliminado_el = null;
+                        db.produccion_diaria.Add(pd);
+                        db.SaveChanges();
+                        MessageBox.Show("Registro Guardado", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                        tb_produccion.Text = "";
+                        tb_id.Text = "";
+                        tb_produccion.Focus();
+                        mostrar_datagridview();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Registro Ya Existe", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    tb_produccion.Text = "";
+                    tb_id.Text = "";
+                    tb_produccion.Focus();
+                    mostrar_datagridview();
+                }
+            }
+        }
 
 
+
+        private void cm_producto_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            cm_unimed.Focus();
+        }
+
+        private void cm_unimed_SelecionChangeCommited(object sender, EventArgs e)
+        {
+            tb_produccion.Focus();
+        }
+
+        private void tb_produccion_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!(char.IsNumber(e.KeyChar)) && (e.KeyChar != (char)Keys.Back))
+            {
+
+                if (e.KeyChar == '\r')
+                {
+                    if (string.IsNullOrEmpty(tb_produccion.Text))
+                    {
+                        MessageBox.Show("Debe Ingresar Produccion", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                        tb_produccion.Focus();
+                    }
+                    else
+                    {
+                        bt_guardar.Focus();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Solo se Permiten Numeros", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    e.Handled = true;
+                    return;
+                }
+
+            }
 
         }
 
-        private void bt_editar_Click(object sender, EventArgs e)
+
+        private void mostrar_datagridview()
         {
+            costeoEntities dbproduccion_diaria = new costeoEntities();
+            DataTable tabla = new DataTable();
+            tabla.Columns.Add("Fecha");
+            tabla.Columns.Add("Producto");
+            tabla.Columns.Add("U.M.");
+            tabla.Columns.Add("Produccion");
+            tabla.Columns.Add("Id");
+            foreach (var dato in dbproduccion_diaria.produccion_diaria.ToList())
+            {
+                if (dato.eliminado_el == null)
+                {
+                    DataRow row = tabla.NewRow();
+                    row["Fecha"] = dato.fecha.ToShortDateString();
+                    costeoEntities db = new costeoEntities();
+                    var producto = db.producto.FirstOrDefault(codigo => codigo.id == dato.producto_id);
+                    row["Producto"] = producto.nombre;
+                    row["U.M."] = dato.unidad_medida.ToString();
+                    row["Produccion"] = dato.produccion.ToString();
+                    row["Id"] = Convert.ToString(dato.id);
+                    tabla.Rows.Add(row);
+                }
+            }
+            tabla.DefaultView.Sort = "[Fecha] DESC";
+            dg_mostrar.DataSource = tabla;
+            dg_mostrar.Columns["Producto"].Width = 226;
+            dg_mostrar.Columns["Id"].Visible = false;
+
+            dg_mostrar.Columns["Fecha"].ReadOnly = true;
+            dg_mostrar.Columns["Producto"].ReadOnly = true;
+            dg_mostrar.Columns["U.M."].ReadOnly = true;
+            dg_mostrar.Columns["Produccion"].ReadOnly = true;
+
+            dg_mostrar.Columns["Fecha"].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dg_mostrar.Columns["Produccion"].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleRight;
+            dg_mostrar.ColumnHeadersDefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dg_mostrar.Refresh();
+
+
 
         }
 
         private void bt_eliminar_Click(object sender, EventArgs e)
         {
+            if (tb_produccion.Text == "")
+            {
+                MessageBox.Show("Para Eliminar, Debe Pinchar Celda de Grilla", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                tb_produccion.Focus();
+            }
+            else
+            {
+                if (tb_id.Text == "")
+                {
+                    MessageBox.Show("ERROR : No Permite Eliminar Datos", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    tb_produccion.Focus();
+                }
+                else
+                {
 
+                    if (MessageBox.Show("Estas seguro de eliminar este registro ?", "Eliminar registro", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                    {
+                        costeoEntities db = new costeoEntities();
+                        produccion_diaria pro = new produccion_diaria();
+                        pro = db.produccion_diaria.Find(Convert.ToInt16(tb_id.Text));
+
+                        if (pro == null)
+                        {
+                            MessageBox.Show("ERROR : No Permite Eliminar Registro", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                            return;
+                        }
+                        else
+                        {
+                            pro.eliminado_el = DateTime.Today;
+                            db.SaveChanges();
+                            MessageBox.Show("Registro Eliminado", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                            tb_produccion.Text = "";
+                            tb_id.Text = "";
+                            tb_produccion.Focus();
+                            mostrar_datagridview();
+                        }
+                    }
+                    else
+                    {
+                        tb_produccion.Text = "";
+                        tb_id.Text = "";
+                        tb_produccion.Focus();
+                        mostrar_datagridview();
+
+                    }
+                }
+            }
         }
 
-        private void dg_reporte_CellContentDoubleClick(object sender, DataGridViewCellEventArgs e)
+        private void dg_mostrar_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
         {
-            //tb_id.Text = dg_mostrar.CurrentRow.Cells["Id"].Value.ToString();
-            //tb_nuevo.Text = dg_mostrar.CurrentRow.Cells["Precio"].Value.ToString();
+            tb_id.Text = dg_mostrar.CurrentRow.Cells["Id"].Value.ToString();
             costeoEntities db = new costeoEntities();
-            produccion_diaria pre = new produccion_diaria();
-            //pre = db.precio_venta.Find(Convert.ToInt16(tb_id.Text));
-            //var codigo_int = Convert.ToInt64(pre.producto_id);
-            //var producto = db.producto.FirstOrDefault(codigo => codigo.id == codigo_int);
-            //costeoEntities dbf = new costeoEntities();
-            //var familia = dbf.familia.FirstOrDefault(codigof => codigof.id == producto.familia_id);
-            //var linea_id = familia.linea_id;
+            produccion_diaria pro = new produccion_diaria();
+            pro = db.produccion_diaria.Find(Convert.ToInt16(tb_id.Text));
 
-            //costeoEntities dbl = new costeoEntities();
-            //var linea = dbl.linea.FirstOrDefault(codigol => codigol.id == linea_id);
-            cm_prodfinal.Text = pre.producto_id.ToString();
-            tb_unidadmedida.Text = pre.unidad_medida.ToString();
-            tb_produccion.Text = pre.produccion.ToString();
-            
+            tb_produccion.Text = Convert.ToString(pro.produccion);
+
+            int index = cm_producto.FindString(dg_mostrar.CurrentRow.Cells["Producto"].Value.ToString());
+            var lista = db.producto.ToList();
+            if (lista.Count > 0)
+            {
+                cm_producto.DataSource = lista;
+                cm_producto.DisplayMember = "nombre";
+                cm_producto.ValueMember = "id";
+            }
+            cm_producto.SelectedIndex = index;
+
+            int indexum = cm_unimed.FindString(dg_mostrar.CurrentRow.Cells["U.M."].Value.ToString());
+            var listaum = db.unidad_medida.ToList();
+            if (listaum.Count > 0)
+            {
+                cm_unimed.DataSource = listaum;
+                cm_unimed.DisplayMember = "descripcion";
+                cm_unimed.ValueMember = "id";
+            }
+            cm_unimed.SelectedIndex = indexum;
         }
 
-        private void tb_id_TextChanged(object sender, EventArgs e)
+        private void bt_editar_Click(object sender, EventArgs e)
         {
+            if (tb_produccion.Text == "")
+            {
+                MessageBox.Show("Para Editar, Debe Pinchar Celda de Grilla", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                tb_produccion.Focus();
+            }
+            else
+            {
+                if (tb_id.Text == "")
+                {
+                    MessageBox.Show("ERROR : No Permite Modificar Datos", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    tb_produccion.Focus();
+                }
+                else
+                {
+                    costeoEntities db = new costeoEntities();
+                    produccion_diaria pd = new produccion_diaria();
+                    pd = db.produccion_diaria.Find(Convert.ToInt16(tb_id.Text));
 
+                    if (pd == null)
+                    {
+                        MessageBox.Show("ERROR : No Permite Modificar Registro", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                        return;
+                    }
+                    else
+                    {
+                        producto pro = new producto();
+                        var codigo_pro = Convert.ToInt16(cm_producto.SelectedValue);
+                        unidad_medida um = new unidad_medida();
+                        var codigo_um = Convert.ToInt16(cm_unimed.SelectedValue);
+                        var unimed = db.unidad_medida.FirstOrDefault(codigol => codigol.id == codigo_um);
+
+                        if ((pd.produccion != Convert.ToString(tb_produccion.Text)) ||
+                            (pd.producto_id != Convert.ToInt16(codigo_pro)) ||
+                            (pd.unidad_medida != Convert.ToString(unimed.descripcion))
+                            )
+                        {
+                            pd.unidad_medida = Convert.ToString(unimed.descripcion);
+                            pd.produccion = Convert.ToString(tb_produccion.Text);
+                            pd.producto_id = Convert.ToInt16(codigo_pro);
+                            pd.modicifado_el = DateTime.Today;
+                            db.SaveChanges();
+                            MessageBox.Show("Registro Modificado", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                            tb_produccion.Text = "";
+                            tb_id.Text = "";
+                            tb_produccion.Focus();
+                            mostrar_datagridview();
+                        }
+                        else
+                        {
+                            MessageBox.Show("No Modificó Datos", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                            tb_produccion.Text = "";
+                            tb_id.Text = "";
+                            tb_produccion.Focus();
+                            mostrar_datagridview();
+                        }
+                    }
+                }
+
+            }
         }
     }
 }
